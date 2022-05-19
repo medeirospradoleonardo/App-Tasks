d29a5ba01fbf3a77e1ddc3ded46ce49901e34473
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-05-19 13:19:27 -0300

    Componente Tarefa #06

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import moment from 'moment'
@@ -9,7 +9,7 @@ import commonStyles from '../commonStyles'
 
 export default props => {
 
-    const doneOrNotStyle = props.doneAt != null ? 
+    const doneOrNotStyle = props.doneAt != null ?
         { textDecorationLine: 'line-through' } : {}
 
     const date = props.doneAt ? props.doneAt : props.estimateAt
@@ -18,9 +18,12 @@ export default props => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.checkContainer}>
-                {getCheckView(props.doneAt)}
-            </View>
+            <TouchableWithoutFeedback
+                onPress={() => props.toggleTask(props.id)}>
+                <View style={styles.checkContainer}>
+                    {getCheckView(props.doneAt)}
+                </View>
+            </TouchableWithoutFeedback>
             <View>
                 <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
                 <Text style={styles.date}>{formattedDate}</Text>
@@ -29,17 +32,17 @@ export default props => {
     )
 }
 
-function getCheckView(doneAt){
-    if(doneAt != null){
-        return(
+function getCheckView(doneAt) {
+    if (doneAt != null) {
+        return (
             <View style={styles.done}>
                 <Icon name='check' size={20}
                     color='#FFF'></Icon>
             </View>
-        )  
+        )
     }
-    else{
-        return(
+    else {
+        return (
             <View style={styles.pending}></View>
         )
     }
@@ -53,19 +56,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingVertical: 10
     },
-    checkContainer:{
+    checkContainer: {
         width: '20%',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    pending:{
+    pending: {
         height: 25,
         width: 25,
         borderRadius: 13,
         borderWidth: 1,
         borderColor: '#555'
     },
-    done:{
+    done: {
         height: 25,
         width: 25,
         borderRadius: 13,
@@ -73,12 +76,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    desc:{
+    desc: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.mainText,
         fontSize: 15
     },
-    date:{
+    date: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.subText
     }
